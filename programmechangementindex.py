a671446c4d947f2c65d74ffb6248d17b2cd548d5
Yupyup <99692735+eliesav@users.noreply.github.com>
2022-04-09 00:24:53 +0200

    Update programmechangementindex.py

--- programmechangementindex.py
+++ programmechangementindex.py
@@ -5,15 +5,20 @@ import re
 os.chdir("C://Users//elisa//Downloads//Enrichissement de corpus//Projet-2//")
 #on va utiliser la technique des buffers pour avoir le contexte avant (histoire de prendre les bons index)
 buf = ["-", "-", "-", "-", "-"] #contexte pour l'indice de début de mot
-buf2 = ["-", "-", "-"] #contexte pour l'indice de fin de mmot
+buf2 = ["-", "-", "-"] #contexte pour l'indice de fin de mot
 buf3 = [] #liste de l'indice de début de mot et fin de mot
+buf4 = ["-", "-"] #permet de ne pas garder la balise </end>
 with open("annotationEvalFinale.xml", "w") as annotationFinale :
     for fichier in listdir("C://Users//elisa//Downloads//Enrichissement de corpus//Projet-2//glozz//") :
         if fichier.endswith(".aa") :
             with open("C://Users//elisa//Downloads//Enrichissement de corpus//Projet-2//glozz//" +fichier, encoding="utf-8") as annotationaa : 
                 for ligne in annotationaa :
+                    buf4.pop(0)
+                    buf4.append("-") #on décale le buffer dans le texte
+                    print(buf2)
+                    print(buf4)
                     #on vérifie que les lignes que l'on ne veut pas ne soit pas écrite
-                    if buf2[0] == "-" and buf2[1] == "-" and buf2[2] == "-" and '</featureSet>' not in buf[0] and '</featureSet>' not in buf[1]  and '</featureSet>' not in buf[2] :
+                    if buf2[0] == "-" and buf2[1] == "-" and buf2[2] == "-" and '</featureSet>' not in buf[0] and '</featureSet>' not in buf[1]  and '</featureSet>' not in buf[2] and not "findemot" in buf4  :
                         annotationFinale.write(ligne)
                     buf.pop(0)
                     categorie = re.match("</featureSet>", ligne)
@@ -25,6 +30,8 @@ with open("annotationEvalFinale.xml", "w") as annotationFinale :
                         positionend = re.match('<singlePosition index="([^<]+?)"/>', ligne)
                         print(positionend.group(1))
                         buf3.append(int(positionend.group(1))) #ajout de l'index de fin de mot dans la liste
+                        buf4.pop(0)
+                        buf4.append("findemot") #on ajoute le contexte pour attraper la balise </end>
                         buf2.pop(0) 
                         buf2.append("-") #on décale le buffer2 dans le texte
                     elif buf2[1] != "-" or buf2[2] != "-" :
@@ -45,8 +52,8 @@ with open("annotationEvalFinale.xml", "w") as annotationFinale :
                                 remplacement = "".join(lettres[buf3[0]:buf3[1]]) #on joint les lettres pour créer le mot final qui remplacera les index de position de mots dans le fichier xml
                         print(remplacement)
                         buf3.pop(0)
-                        buf3.pop(0)
-                        annotationFinale.write(f'<singlePosition index="{remplacement}"/>')
+                        buf3.pop(0) #on remet à 0 la liste de début et fin de mot
+                        annotationFinale.write(f'<singlePosition index="{remplacement}"/>\n')
 
 #### PROBLEMES
 #1. Les mots sortis des index="" sont pas les bons --> peut-être un problème au niveau de l'extraction du texte lettre par lettre? (genre il prend pas les caractères non printable?)
