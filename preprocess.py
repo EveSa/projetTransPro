c7eb3faf3e8c5b591203d800833b760f29d47172
Eve <sauvageve@gmail.com>
2022-04-22 12:34:29 +0200

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -7,16 +7,20 @@ corpus_xml=open("corpus_final_annote.xml.aa").read()
 #Et le corpus de référence
 corpus=open("corpus_final_annote.txt.ac").read()
 
-nlp = spacy.blank("en")
+nlp = spacy.blank("fr")
 training_data = annotations(corpus_xml,corpus)
 # the DocBin will store the example documents
 db = DocBin()
 for text, annotation in training_data:
+    print(text)
+    print(annotation)
     doc = nlp(text)
     ents = []
     for start, end, label in annotation:
         span = doc.char_span(start, end, label=label)
-        ents.append(span)
+        if span!=None :
+            ents.append(span)
+        print(ents)
     doc.ents = ents
     db.add(doc)
 db.to_disk("./train.spacy")
